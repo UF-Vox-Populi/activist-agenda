905cd71b9bc52c3c045a88348c0059131cafb986
Laurence <laurencemullen99@gmail.com>
2020-07-15 19:46:10 -0400

    Feat loginpg (#2)
    * Created Login page from material-ui template
    
    * Theme color updated
    
    * Express server and mongo database
    
    * Git Fix
    
    * Added some comments
    
    * Added functionality to handle fields and submit
    
    * Redirect and imports
    
    * Login updated to match release branch
    
    * Empty field styling
    
    * Added comments
    
    * Fixed field state updates, button styling changes
    
    * remember cookie functionality v1
    
    * Remember and cookie functionality updated v2

--- activist-agenda/src/App.js
+++ activist-agenda/src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'fontsource-roboto';
+import Login from './components/Login';
+import { MuiThemeProvider } from '@material-ui/core/styles'; // Allows custom color theme
+import theme from './theme'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {/* Wrapper for custom theme */}
+      <MuiThemeProvider theme={theme}>
+        <Login />
+      </MuiThemeProvider>
       </header>
     </div>
   );
