daf0c4dcfd869c68f76af3aad1acfd3f819d5850
Laurence Mullen <laurencemullen99@gmail.com>
2020-08-12 18:59:32 -0400

    Commented out fillers

--- server/server.js
+++ server/server.js
@@ -24,95 +24,95 @@ function handleError(err,res) {
 
 
 //Deletes all entries. Basically a reset, just used for testing purposes at the moment.
-User.deleteMany({}, (err) => {
-    if (err) throw err;
-});
+// User.deleteMany({}, (err) => {
+//     if (err) throw err;
+// });
 
-Post.deleteMany({}, (err) => {
-    if (err) throw err;
-});
-Token.deleteMany({}, (err) => {
-    if (err) throw err;
-});
+// Post.deleteMany({}, (err) => {
+//     if (err) throw err;
+// });
+// Token.deleteMany({}, (err) => {
+//     if (err) throw err;
+// });
 
-//Reads the Filler Users json and puts in all of us for testing purposes.
-fs.readFile('server/database/FillerUsers.json', 'utf8', (err, data) => {
-    if (err) throw err;
-    let userData = JSON.parse(data);
+// //Reads the Filler Users json and puts in all of us for testing purposes.
+// fs.readFile('server/database/FillerUsers.json', 'utf8', (err, data) => {
+//     if (err) throw err;
+//     let userData = JSON.parse(data);
 
-    userData.entries.forEach(element => {
-        var thing = new User({username:element.username, email:element.email, password:element.password, firstName:element.firstName, lastName:element.lastName, bio:element.bio, location:element.location, organizer:element.organizer, emailVerified:element.emailVerified, authLevel:element.authLevel});
-        thing.save(function (err) {
-            if (err) {
-              return handleError(err);
-            }
-        })
-    }, () => {
-        mongoose.connection.close();
-    });
-});
+//     userData.entries.forEach(element => {
+//         var thing = new User({username:element.username, email:element.email, password:element.password, firstName:element.firstName, lastName:element.lastName, bio:element.bio, location:element.location, organizer:element.organizer, emailVerified:element.emailVerified, authLevel:element.authLevel});
+//         thing.save(function (err) {
+//             if (err) {
+//               return handleError(err);
+//             }
+//         })
+//     }, () => {
+//         mongoose.connection.close();
+//     });
+// });
 
-fs.readFile('server/database/fillerEvents.json', 'utf8', (err, data) => {
-    mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+// fs.readFile('server/database/fillerEvents.json', 'utf8', (err, data) => {
+//     mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
     
-    //Reset
-    Event.deleteMany({}, (err) => {
-        if (err) throw err;
-    });
+//     //Reset
+//     Event.deleteMany({}, (err) => {
+//         if (err) throw err;
+//     });
     
-    console.log("Filling EventDB")
-    if (err) throw err;
-    let eventData = JSON.parse(data);
+//     console.log("Filling EventDB")
+//     if (err) throw err;
+//     let eventData = JSON.parse(data);
 
-    eventData.entries.forEach(element => {
-        var thing = new Event({summary:element.summary, address:element.address, coordinates:element.coordinates, date:element.date, description:element.description});
-    thing.save(function (err) {
-            if (err) {
-              throw err;
-            }
-        })
-    }, () => {
-        mongoose.connection.close();
-    });
-});
+//     eventData.entries.forEach(element => {
+//         var thing = new Event({summary:element.summary, address:element.address, coordinates:element.coordinates, date:element.date, description:element.description});
+//     thing.save(function (err) {
+//             if (err) {
+//               throw err;
+//             }
+//         })
+//     }, () => {
+//         mongoose.connection.close();
+//     });
+// });
     
 
-fs.readFile('server/database/FillerPosts.json', 'utf8', (err, data) => {
-    mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
-    if (err) throw err;
-    let postData = JSON.parse(data);
+// fs.readFile('server/database/FillerPosts.json', 'utf8', (err, data) => {
+//     mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true});
+//     if (err) throw err;
+//     let postData = JSON.parse(data);
 
-    console.log("Filling PostDB")
+//     console.log("Filling PostDB")
 
 
-    postData.posts.forEach(element => {
-        var thing = new Post({
-            poster: element.poster,
-            posterID: element.posterID,
-            icon: element.icon,
-            title: element.title,
-            donationLink: element.donationLink,
-            organizationLink: element.organizationLink,
-            description: element.description,
-            time: element.time,
-            address: element.address,
-            coordinates: {
-                latitude: element.coordinates.latitude,
-                longitude: element.coordinates.longitude
-            },
-            isEvent: element.isEvent,
-            supporters: element.supporters,
-            flagged: element.flagged
-        });
-        thing.save(function (err) {
-            if (err) {
-              throw err;
-            }
-        })
-    }, () => {
-        mongoose.connection.close();
-    });
-});
+//     postData.posts.forEach(element => {
+//         var thing = new Post({
+//             poster: element.poster,
+//             posterID: element.posterID,
+//             icon: element.icon,
+//             title: element.title,
+//             donationLink: element.donationLink,
+//             organizationLink: element.organizationLink,
+//             description: element.description,
+//             time: element.time,
+//             address: element.address,
+//             coordinates: {
+//                 latitude: element.coordinates.latitude,
+//                 longitude: element.coordinates.longitude
+//             },
+//             isEvent: element.isEvent,
+//             supporters: element.supporters,
+//             flagged: element.flagged
+//         });
+//         thing.save(function (err) {
+//             if (err) {
+//               throw err;
+//             }
+//         })
+//     }, () => {
+//         mongoose.connection.close();
+//     });
+// });
 
 //This is needed to run the Express app.
 const app = express();
