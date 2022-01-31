d4170f5dc98493249e6d7826f5eec0cd4de65d8c
Laurence <laurencemullen99@gmail.com>
2022-01-31 17:25:01 -0500

    Remove mailgun API key

--- activist-agenda/src/mailgun.js
+++ activist-agenda/src/mailgun.js
@@ -1,6 +1,6 @@
 const mailgun = require("mailgun-js");
 
-var mgAPI = process.env.MG_API || '89ba1a16d628f9c38d3fc47828016993-ffefc4e4-74054983';
+var mgAPI = process.env.MG_API || '';
 var mgDOMAIN = process.env.MG_DOMAIN || 'mg.activistagenda.vision';
 var mg = mailgun({apiKey: mgAPI, domain: mgDOMAIN});
 
